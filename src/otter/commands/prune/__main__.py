9e2d5e437d2a31998b277c02220df88713e8ed9f
Nate Tangsurat <e4r7hbug@gmail.com>
2018-06-06 16:18:26 -0500

    feat: Pass in Wheels directory

--- src/otter/commands/prune/__main__.py
+++ src/otter/commands/prune/__main__.py
@@ -1,10 +1,15 @@
 """Otter Pilot command prune."""
 import logging
 
+import click
+
 LOG = logging.getLogger(__name__)
 
 
-def main():
+@click.command()
+@click.option(
+    '-w', '--wheel-dir', type=click.Path(exists=True, file_okay=False), required=True, help='Location of Wheels.')
+def main(wheel_dir):
     """Otter Pilot prune.
 
     Good for taking care of business.
